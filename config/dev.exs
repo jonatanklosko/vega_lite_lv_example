70fd02481c795f7eed1875d1d4cd2a71279c08d2
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-07-21 18:32:31 +0200

    Migrate to Webpack 5

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,8 @@ config :vega_lite_lv_example, VegaLiteLvExampleWeb.Endpoint,
       "node_modules/webpack/bin/webpack.js",
       "--mode",
       "development",
-      "--watch-stdin",
+      "--watch",
+      "--watch-options-stdin",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
